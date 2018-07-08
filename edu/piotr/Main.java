bbac9278c2ccbf649d80410a95dac37c96522d45
Piotr Wolak <piotrwolakk@gmail.com>
2018-07-09 00:43:13 +0200

    Some minor bugs has been fixed, event2() added, events now happens randomly thanks to pickEvent(), fixed the bug which sets the level of farm to negative after event1 happened

--- edu/piotr/Main.java
+++ edu/piotr/Main.java
@@ -1,6 +1,7 @@
 package edu.piotr;
 
 import java.util.InputMismatchException;
+import java.util.Random;
 import java.util.Scanner;
 
 import static java.lang.System.exit;
@@ -15,9 +16,10 @@ public class Main {
 
         Village village = new Village(villageName);
         int round = 0;
+        showMenu();
         while (true) {
             round++;
-            if (round % 10 == 0) event1(village, round);
+            if (round % 10 == 0)pickEvent(village,round);
             else menu(village);
         }
     }
@@ -46,10 +48,12 @@ public class Main {
     }
 
     public static void menu(Village village) {
-        System.out.println("What would you like to do ?");
-        showMenu();
+        System.out.println("What would you like to do ? Press 0 if you want to see menu");
         int choice = getChoiceMenu();
         switch (choice) {
+            case 0:
+                showMenu();
+                break;
             case 1:
                 if (village.upgrade(village.getFarm()))
                     System.out.println("Farm has been upgraded to level" + village.getFarm().getLevel());
@@ -57,7 +61,7 @@ public class Main {
                 break;
             case 2:
                 if (village.upgrade(village.getTower()))
-                    System.out.println("Tower has been upgraded to level" + village.getFarm().getLevel());
+                    System.out.println("Tower has been upgraded to level" + village.getTower().getLevel());
                 else System.out.println("Tower has not been upgraded");
                 break;
             case 3:
@@ -71,20 +75,41 @@ public class Main {
                 exit(0);
                 break;
             default:
-                exit(0);
+                showMenu();
         }
 
+    }
+    public static void pickEvent(Village village, int i){
+        Random generator = new Random();
+        boolean random = generator.nextBoolean();
+        if(random)event1(village, i);
+        else event2(village, i);
+    }
+    public static void event2(Village village, int i){
+        System.out.println("Farm on level " + village.getFarm().getLevel());
+        System.out.println("Tower on level " + village.getTower().getLevel());
+        System.out.println("An earthquake occured ! Your facilities has been damaged");
+        village.getTower().setLevel((int)village.getTower().getLevel()/10);
+        village.getFarm().setLevel((int)village.getFarm().getLevel()/10);
+        System.out.println("Farm on level " + village.getFarm().getLevel());
+        System.out.println("Tower on level " + village.getTower().getLevel());
     }
 
     public static void event1(Village village, int i) {
         System.out.println("You Village has been attacked by savages, here's what happened");
-        if (village.getTower().getLevel() < (int) i / 2) {
+        if (village.getTower().getLevel() < (int) i / 5) {
             int damage = (i / 2) - village.getTower().getLevel();
             System.out.println("your farm is on " + village.getFarm().getLevel() + " level");
-            System.out.println("Barbarians has pwned you and decreased your farm level by " + damage);
-            village.getFarm().setLevel(village.getFarm().getLevel() - damage);
+            if(village.getFarm().getLevel()>damage){
+                System.out.println("Barbarians has pwned you and decreased your farm level by " + damage);
+                village.getFarm().setLevel(village.getFarm().getLevel() - damage);
+            }
+            else {
+                System.out.println("Savages destroyed your farm completly");
+                village.getFarm().setLevel(1);
+            }
             System.out.println("your farm is on " + village.getFarm().getLevel() + " level");
-        } else if (village.getTower().getLevel() >= (int) i / 2) {
+        } else if (village.getTower().getLevel() >= (int) i / 5) {
             System.out.println("You have defended against Barbarians, well done");
         }
     }
--- edu/piotr/Village.java
+++ edu/piotr/Village.java
@@ -22,24 +22,24 @@ public class Village {
 
         int cost = 10 * facility.getLevel();
         System.out.println("The  cost of upgrading " + facility.getName() + " is " + cost + "gold");
-        System.out.println("Do you want to proceed ? 1- yes, 2-not");
+        System.out.println("Do you want to proceed ? 1- yes, 2-no");
         int choice = Main.getChoiceMenu();
         switch (choice) {
             case 1:
                 if (this.getGold() >= cost) {
                     this.setGold(this.getGold() - cost);
                     facility.upgrade();
+                    return true;
                 } else {
                     System.out.println("Not enough gold");
                     return false;
                 }
-                break;
             case 2:
                 return false;
             default:
                 return false;
         }
-        return false;
+
     }
 
     public class MakeMoney extends Thread {
